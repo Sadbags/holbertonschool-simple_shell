320d0dc4b1af9b3cbd825559f7250762e2d4c1a1
GinnaVionette <9011@holbertonstudents.com>
2024-04-15 12:26:33 -0400

    trying out new code

--- shell.c
+++ shell.c
@@ -1,31 +1,78 @@
 #include "shell.h"
 
 /**
- * main - Entry point
+ * display_prompt - Displays the shell prompt
+ */
+void display_prompt(void);
+
+/**
+ * main - Entry point of the shell program
  *
- * Return: Always 0 (success)
-*/
+ * Return: Always 0
+ */
 int main(void)
 {
-	char *input = NULL;
-	size_t len = 0;
-	ssize_t read;
+    char input[MAX_INPUT_LENGTH];
+    char *args[MAX_ARGS];
+    int num_args;
+    char *token;
+    pid_t pid;
 
-	while (1)
-	{
-		printf("simple_shell ");
-		read = getline(&input, &len, stdin);
+    while (1)
+    {
+        display_prompt();
 
-		if (read == -1)
-		{
-			perror("getline");
-			exit(EXIT_SUCCESS);
-		}
-		if (strcmp(input, "exit\n") == 0)
-		free(input);
-		exit(EXIT_FAILURE);
+        if (fgets(input, MAX_INPUT_LENGTH, stdin) == NULL)
+        {
+            printf("\n");
+            exit(EXIT_SUCCESS);
+        }
 
-	}
-	free(input);
-	return (0);
+        input[strcspn(input, "\n")] = '\0';
+
+        if (strcmp(input, "exit") == 0)
+        {
+            exit(EXIT_SUCCESS);
+        }
+
+        num_args = 0;
+        token = strtok(input, " ");
+        while (token != NULL && num_args < MAX_ARGS - 1)
+        {
+            args[num_args++] = token;
+            token = strtok(NULL, " ");
+        }
+        args[num_args] = NULL;
+
+        pid = fork();
+        if (pid == -1)
+        {
+            perror("fork");
+            exit(EXIT_FAILURE);
+        }
+        else if (pid == 0)
+        {
+            if (execvp(args[0], args) == -1)
+            {
+                perror("execvp");
+                exit(EXIT_FAILURE);
+            }
+        }
+        else
+        {
+            int status;
+            waitpid(pid, &status, 0);
+        }
+    }
+
+    return 0;
+}
+
+/**
+ * display_prompt - Displays the shell prompt
+ */
+void display_prompt(void)
+{
+    char display_prompt[] = "$ ";
+    write(STDOUT_FILENO, display_prompt, 2);
 }
