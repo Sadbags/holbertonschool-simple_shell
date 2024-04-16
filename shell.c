579b64ffee3f1199ec54873394700c8cb469b2a4
Bryan Garcia <bags@Bryans-MacBook-Air.local>
2024-04-16 18:27:27 -0400

    fixing bug

--- shell.c
+++ shell.c
@@ -1,19 +1,24 @@
 #include "shell.h"
 
+/**
+ * main - Entry point of the simple shell program
+ *
+ * Return: Always 0
+*/
 int main(void)
 {
-    char *input;
-    char **args;
+char *input;
+char **args;
 
-    while (1)
-	{
-        display_prompt();
-        input = read_input();
-        args = parse_input(input);
-        execute_command(args);
-        free(input);
-        free(args);
-    }
+while (1)
+{
+display_prompt();
+input = read_input();
+args = parse_input(input);
+execute_command(args);
+free(input);
+free(args);
+}
 
-    return 0;
+return (0);
 }
