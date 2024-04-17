562579a81aa17ea5325c13246816d61f16deae65
GinnaVionette <9011@holbertonstudents.com>
2024-04-17 08:51:56 -0400

    new code to test

--- shell_functions.c
+++ shell_functions.c
@@ -1,98 +1,76 @@
-#include "shell.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 #define MAX_INPUT_LENGTH 1024
 
-/**
- * display_prompt - Displays the shell prompt
- */
-void display_prompt(void)
-{
-printf("$ ");
-}
-
-/**
- * read_input - Reads input from the user
- *
- * Return: Pointer to the input string
- */
-char *read_input(void)
-{
-char *input = malloc(MAX_INPUT_LENGTH);
-if (!input)
-{
-perror("malloc failed");
-exit(EXIT_FAILURE);
-}
-
-if (fgets(input, MAX_INPUT_LENGTH, stdin) == NULL)
-{
-printf("\n");
-free(input);
-exit(EXIT_SUCCESS);
-}
-
-input[strcspn(input, "\n")] = '\0';
-return (input);
-}
-
-/**
- * parse_input - Parses input into tokens
- * @input: Input string to be parsed
- *
- * Return: Array of pointers to tokens
- */
 char **parse_input(char *input)
 {
-char **args = malloc(MAX_INPUT_LENGTH * sizeof(char *));
-char *token;
-int i = 0;
+    char **args = malloc(MAX_INPUT_LENGTH * sizeof(char *));
+    char *token;
+    int i = 0;
 
-if (!args)
-{
-perror("malloc failed");
-exit(EXIT_FAILURE);
+    if (!args)
+    {
+        perror("malloc failed");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(input, " ");
+    while (token != NULL)
+    {
+        args[i++] = token;
+        token = strtok(NULL, " ");
+    }
+    args[i] = NULL;
+
+    return args;
 }
 
-token = strtok(input, " ");
-while (token != NULL)
-{
-args[i++] = token;
-token = strtok(NULL, " ");
-}
-args[i] = NULL;
-
-return (args);
-}
-
-/**
- * execute_command - Executes the command
- * @args: Array of command and arguments
- */
 void execute_command(char **args)
 {
-pid_t pid;
-int status;
+    pid_t pid;
+    int status;
 
-pid = fork();
-if (pid < 0)
+    pid = fork();
+    if (pid < 0)
+    {
+        perror("fork failed");
+        exit(EXIT_FAILURE);
+    }
+    else if (pid == 0)
+    {
+        if (execvp(args[0], args) == -1)
+        {
+            perror("execvp failed");
+            exit(EXIT_FAILURE);
+        }
+    }
+    else
+    {
+        waitpid(pid, &status, 0);
+    }
+}
+
+int main()
 {
-perror("fork failed");
-exit(EXIT_FAILURE);
+    char **args;
+    char input[MAX_INPUT_LENGTH];
+
+    printf("Enter command: ");
+    fgets(input, sizeof(input), stdin);
+
+    /* Remove trailing newline character */
+    if ((strlen(input) > 0) && (input[strlen(input) - 1] == '\n'))
+        input[strlen(input) - 1] = '\0';
+
+    args = parse_input(input);
+    execute_command(args);
+
+    free(args);
+
+    return 0;
 }
-else if (pid == 0)
-{
-if (args[0] != NULL && strcmp(args[0], "exit") == 0)
-{
-exit(EXIT_SUCCESS);
-}
-if (execvp(args[0], args) == -1)
-{
-perror("execvp failed");
-exit(EXIT_FAILURE);
-}
-}
-else
-{
-waitpid(pid, &status, 0);
-}
-}
+
