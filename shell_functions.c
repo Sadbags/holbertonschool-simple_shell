53d8259c9792014783bf2f5c9af253ee5963b8ec
Bryan Garcia <bags@Bryans-Air.localdomain>
2024-04-22 12:02:55 -0400

    moved execute functions

--- shell_functions.c
+++ shell_functions.c
@@ -1,12 +1,13 @@
 #include "shell.h"
 
 #define MAX_INPUT_LENGTH 1024
+
 /**
  * display_prompt - Displays the shell prompt
  */
 void display_prompt(void)
 {
-	printf("simple_shell$ ");
+printf("simple_shell$ ");
 }
 
 /**
@@ -16,29 +17,22 @@ void display_prompt(void)
  */
 char *read_input(void)
 {
-	char *input = (char *)malloc(MAX_INPUT_LENGTH * sizeof(char));
-	unsigned int len;
+char *input = malloc(MAX_INPUT_LENGTH);
+if (!input)
+{
+perror("malloc failed");
+exit(EXIT_FAILURE);
+}
 
-	if (!input)
-	{
-		perror("malloc failed");
-		exit(EXIT_FAILURE);
-	}
+if (fgets(input, MAX_INPUT_LENGTH, stdin) == NULL)
+{
+printf("\n");
+free(input);
+exit(EXIT_SUCCESS);
+}
 
-	if (fgets(input, MAX_INPUT_LENGTH, stdin) == NULL)
-	{
-		printf("\n");
-		free(input);
-		exit(EXIT_SUCCESS);
-	}
-
-	len = strlen(input);
-	if (len > 0 && input[len - 1] == '\n')
-	{
-		input[len - 1] = '\0';
-	}
-
-	return input;
+input[strcspn(input, "\n")] = '\0';
+return (input);
 }
 
 /**
@@ -49,47 +43,26 @@ char *read_input(void)
  */
 char **parse_input(char *input)
 {
-    char **args = malloc(MAX_INPUT_LENGTH * sizeof(char *));
-	int in_quote = 0;
-	char *start = input;
-    int i = 0;
+char **args = malloc(MAX_INPUT_LENGTH * sizeof(char *));
+char *token;
+int i = 0;
 
-    if (!args)
-    {
-        perror("malloc failed");
-        exit(EXIT_FAILURE);
-    }
-
-    while (*input != '\0')
-    {
-        if (*input == '"' || *input == '\'')
-        {
-            /* Toggle in_quote flag when encountering a quote character */
-            in_quote = !in_quote;
-            input++;
-            continue;
-        }
-
-        if (*input == ' ' && !in_quote)
-        {
-            *input = '\0'; /* Replace space with null terminator */
-            args[i++] = start;
-            start = input + 1; /* Move start pointer to next character */
-        }
-
-        input++;
-    }
-
-    /* Add the last token if it's not empty */
-    if (start != input)
-    {
-        args[i++] = start;
-    }
-
-    args[i] = NULL; /* Terminate the array with a NULL pointer */
-    return args;
+if (!args)
+{
+perror("malloc failed");
+exit(EXIT_FAILURE);
 }
 
+token = strtok(input, " ");
+while (token != NULL)
+{
+args[i++] = token;
+token = strtok(NULL, " ");
+}
+args[i] = NULL;
+
+return (args);
+}
 
 /**
  * strtok - Parses a string into tokens
@@ -100,7 +73,7 @@ char **parse_input(char *input)
  */
 char *strtok(char *str, const char *delim)
 {
-static char *buffer = NULL;
+static char *buffer;
 char *start;
 char *end;
 
@@ -125,75 +98,4 @@ end++;
 
 buffer = NULL;
 return (start);
-}
-
-/**
- * execute_command - Executes the command provided by the user
- * @args: Array of strings containing the command and its arguments
- *
- * Return: 1 on success, -1 on failure
- */
-int execute_command(char **args)
-{
-    pid_t pid;
-    int status;
-
-    if (args == NULL || args[0] == NULL)
-    {
-        /* No command provided */
-        return (1); /* Continue shell loop */
-    }
-
-    if (strcmp(args[0], "exit") == 0)
-    {
-        /* "exit" command detected */
-        exit(EXIT_SUCCESS);
-    }
-
-    if (strcmp(args[0], "cd") == 0)
-    {
-        /* "cd" command detected */
-        if (args[1] == NULL)
-        {
-            fprintf(stderr, "cd: missing argument\n");
-            return (-1); /* Failure */
-        }
-
-        if (chdir(args[1]) != 0)
-        {
-            perror("chdir failed");
-            return (-1); /* Failure */
-        }
-        return (1); /* Success, continue shell loop */
-    }
-
-    /* Check if the command exists in PATH */
-    if (access(args[0], X_OK) == -1)
-    {
-        fprintf(stderr, "%s: command not found\n", args[0]);
-        return (-1); /* Failure */
-    }
-
-    pid = fork();
-    if (pid < 0)
-    {
-        perror("fork failed");
-        exit(EXIT_FAILURE);
-    }
-    else if (pid == 0)
-    {
-        /* Child process */
-        if (execvp(args[0], args) == -1)
-        {
-            perror("execvp failed");
-            exit(EXIT_FAILURE);
-        }
-    }
-    else
-    {
-        /* Parent process */
-        waitpid(pid, &status, 0);
-    }
-
-    return (1); /* Continue shell loop */
 }
