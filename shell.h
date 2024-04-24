cc925707592f6147f3f988e3c354618a55d1de0f
Bryan Garcia <bags@Bryans-Air.localdomain>
2024-04-24 11:48:08 -0400

    added prototypes

--- shell.h
+++ shell.h
@@ -1,38 +1,31 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#define MAX_INPUT_LENGTH 1024
-extern char **environ;
-#define PROMPT "simple_shell "
-
-/* Libraries */
 #include <stdio.h>
-#include <string.h>
 #include <unistd.h>
+#include <string.h>
 #include <stdlib.h>
 #include <sys/types.h>
-#include <sys/stat.h>
 #include <sys/wait.h>
-#include <errno.h>
-#include <string.h>
-#include <stddef.h>
+#include <sys/stat.h>
 
-/* Prototypes */
-void display_prompt(void);
-char *read_input(void);
-char **parse_input(char *input);
-char *strtok(char *str, const char *delim);
+extern char **environ;
+#define MAX_INPUT_SIZE 1024
+#define MAX_CMD_SIZE 1024
+#define DELIM " \t\r\n\a"
+#define PROMPT "simple_shell "
+
 int print_env(void);
-int str_cmp(const char *s1, const char *s2, int n);
+int _str_n_cmp(char *s1, char *s2, int n);
+int _strcmp(char *s1, char *s2);
+int _cmdread(char *s, size_t __attribute__((unused)) file_stream, char *name);
+int _strlen(char *s);
+int _callcommand(char *cmd_arr[], char *name);
+char *_strdup(char *string);
+char *path_finder(char *cmd);
+char *_strcat(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
 char *get_env(char *var);
-char *path_command(const char *cmd);
-int execute_external_command(char **args);
-int execute_command(char **args);
-int execute_cd_command(char **args);
-int execute_builtin_command(char **args);
-int handle_exit_command(void);
-int handle_cd_command(char **args);
-int handle_env_command(void);
-
+void print_not_found(char *cmd, char *name);
 
 #endif
